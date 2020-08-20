a9a30ddeb92c49914592b3433f096b8a0d9e4bb8
Michelle Tanoto <michelletanoto@Michelles-MBP.lan>
2020-08-20 17:01:37 +1000

    add: mock data in locally

---
+++ src/data/mockAdmin.js
@@ -0,0 +1,13 @@
+export default [ 
+    {
+        "firstName" : "John",
+         "lastName" : "Doe",
+         "email" : "johndoe@gmail.com",
+         "username" : "John_admin" ,"password" : "abcd1234" 
+    },
+    {
+        "firstName" : "Frank",
+         "email" : "frank0@gmail.com",
+         "username" : "Frank_admin" ,"password" : "abcd1234"
+    }
+]
