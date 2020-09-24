e088dcd638846d07e485fc160c1f35ed8275d649
Frank-326 <jiangjiahao0326@gmail.com>
2020-09-24 19:17:18 +1000

    added account button / modified update functions

--- src/pages/User/edit.js
+++ src/pages/User/edit.js
@@ -1,133 +1,127 @@
+// Admin can edit registered users after they are logged in
 import React, { useState, useEffect } from 'react';
 import {
   makeStyles
 } from '@material-ui/core';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-// import Snackbar from '@material-ui/core/Snackbar';
-// import Alert from '@material-ui/lab/Alert';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 import axios from 'axios';
   
 const useStyles = makeStyles({
-    // root: {
-    //     '& .MuiTextField-root': {
-    //       margin: theme.spacing(1),
-    //       width: '25ch',
-    //     },
-    // }
+    button: {
+      margin: 10
+    }
 });
 
 const Edit = (props) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
   const [address, setAddress] = useState('');
-  const [open, setOpen] = React.useState(false);
+  const [message, setMessage] = useState('') // error message
+  const [open, setOpen] = useState(false) // open prompt
 
+  // edit specific user as an admin
   useEffect(() => {
     axios
     .get(`http://localhost:3001/users/read`)
     .then(response => {
-      console.log('Yeet user are fetched!')
+      console.log('User are fetched successfully!')
       console.log(response.data)
       setFirstName(response.data[0].firstName)
       setLastName(response.data[0].lastName)
       setEmail(response.data[0].email)
-      setUsername(response.data[0].username)
       setAddress(response.data[0].address)
     })
   }, [])
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+  // update specific user as an admin
+  const updateUser = (event) => {
+    event.preventDefault()
 
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
+    // updated user object
+    const updateUser = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      address: address
     }
 
-    setOpen(false);
-  };
-
-  const updateUser = (event) => {
-      event.preventDefault()
-    const updateUser = {
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          username: username,
-          address: address
-      }
-
     axios
-    .put(`http://localhost:3001/users/update/${props.match.params.id}`, { user: updateUser })
+    .put(`http://localhost:3001/users/update/${props.match.params.id}`, { user: updateUser } )
     .then(response => {
-      console.log('User are updated successsfully')
+      console.log('personal details updated successfully')
       window.location='/user/edit'
     })
+    .catch(error => {
+      setMessage('Failed to update personal details');
+      setOpen(true)
+    })
   }
-
-    const classes = useStyles();  
-
-        return (
-            <form noValidate autoComplete="off">
-            <div>
-            <TextField 
-            required 
-            id="standard-required" 
-            label="First name" 
-            defaultValue="None" 
-            value={firstName} 
-            onChange={(event) => setFirstName(event.target.value)}/>
-            <br />
-            <TextField 
-            required 
-            id="standard" 
-            label="Last name"
-            defaultValue="None" 
-            value={lastName} 
-            onChange={(event) => setLastName(event.target.value)} />
-            <br />
-            <TextField 
-            required 
-            id="standard-required" 
-            label="Email" 
-            defaultValue="None" 
-            value={email} 
-            onChange={(event) => setEmail(event.target.value)} />
-            <br />
-            <TextField 
-            required 
-            id="standard-required" 
-            label="Username" 
-            defaultValue="None" 
-            value={username} 
-            onChange={(event) => setUsername(event.target.value)} />
-            <br />
-            <TextField 
-            required 
-            id="standard" 
-            label="Address" 
-            defaultValue="None" 
-            value={address}
-            onChange={(event) => setAddress(event.target.value)} />
-            <br />
-            <Button variant="contained" color="primary" onClick={(event) => updateUser(event)}>
-               Save
-             </Button>
-             <Button variant="contained" color="secondary" href={`/users/read`}>
-               Cancel
+  
+  const classes = useStyles();  
+  return (
+      <form noValidate autoComplete="off">
+        <div>
+          <TextField 
+          required 
+          id="standard-required" 
+          label="First name" 
+          defaultValue="None" 
+          value={firstName} 
+          onChange={(event) => setFirstName(event.target.value)}/>
+          <br />
+          <TextField 
+          required 
+          id="standard" 
+          label="Last name"
+          defaultValue="None" 
+          value={lastName} 
+          onChange={(event) => setLastName(event.target.value)} />
+          <br />
+          <TextField 
+          required 
+          id="standard-required" 
+          label="Email" 
+          defaultValue="None" 
+          value={email} 
+          onChange={(event) => setEmail(event.target.value)} />
+          <br />
+          <TextField 
+          required 
+          id="standard" 
+          label="Address" 
+          defaultValue="None" 
+          value={address}
+          onChange={(event) => setAddress(event.target.value)} />
+          <br />
+      <Button className={classes.button} variant="contained" color="primary" onClick={(event) => updateUser(event)}>Save</Button>
+      <Button className={classes.button} variant="contained" color="secondary" href={`/user/read`}>Cancel</Button>
+          <Dialog
+          open={open}
+          onClose={() => setOpen(false)}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title">Error</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {message}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpen(false)} color="primary">
+              Okay
             </Button>
-            {/* <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success">
-                    User is updated successfully!
-                </Alert>
-              </Snackbar> */}
-            </div>
-            </form>
-        );
+          </DialogActions>
+        </Dialog>
+      </div>
+      </form>
+      );
 }
 
 export default Edit;
