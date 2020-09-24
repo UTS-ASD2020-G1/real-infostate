9a371cc67a7ea3ca2d68a2dbf39491598b3e2c45
jackGiant24 <felix240600@gmail.com>
2020-09-24 14:37:24 +0800

    Comment and cleanup for Create user
    Commented and cleanup code on Create backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -17,12 +17,18 @@ userRouter.route('/').get(async (req, res, next) => {
   // .catch(error => next(error))
 });
 
+//Create User Route
 userRouter.post(
   '/create',
   [
+    //Check and validate fields
+    //Check firstName is not empty, if not give appropriate response
     check('firstName', 'Please enter your fistname').not().isEmpty(),
+    //Check username is not empty, if not give appropriate response
     check('username', 'Please create a username').not().isEmpty(),
+    //Check if it's a valid email address, if not give appropriate response
     check('email', 'Please enter a valid email address').isEmail(),
+    //Check if the password is more than 6 characters long, if not give appropriate response
     check(
       'password',
       'Please enter a password with 6 or more characters'
@@ -37,6 +43,7 @@ userRouter.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
+    //Get the fields from the request body
     const {
       firstName,
       lastName,
@@ -46,30 +53,45 @@ userRouter.post(
       email,
     } = req.body;
 
+    //Store in userfields variable if the field exist
     const userFields = {};
+    //if there is a username store it as username is userFields
     if (username) userFields.username = username;
+    //if there is a firstName store it as firstName is userFields
     if (firstName) userFields.firstName = firstName;
+    //if there is a lastName store it as lastName is userFields
     if (lastName) userFields.lastName = lastName;
+    //if there is a address store it as address is userFields
     if (address) userFields.address = address;
+    //if there is a password store it as password is userFields
     if (password) userFields.password = password;
+    //if there is a email store it as email is userFields
     if (email) userFields.email = email;
 
     try {
+      //check if there is a user with the inputed username
       let user = await User.findOne({ username });
 
+      //Don ot make if username exist and give appropriate response
       if (user) {
         return res
           .status(400)
           .json({ errors: [{ msg: 'User already exist' }] });
       }
 
+      //Create User object with userFields as attributes
       user = new User(userFields);
+      //Encrypt password
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+      //Save User object to database
       await user.save();
+      //Return the user details
       return res.json(user);
     } catch (err) {
+      //Catch error and display err paramater
       console.error(err.message);
+      //Send status 500 it there is an error
       res.status(500).send('Server Error');
     }
   }
@@ -113,30 +135,28 @@ userRouter.delete('/delete', async (req, res) => {
   }
 });
 
-userRouter.put("/update/:id", async (req, res) => {
-User.findByIdAndUpdate(
-  req.params.id,
-  {
-    $set: {
-      firstName: req.body.firstName, 
-      lastName: req.body.lastName,
-      email: req.body.email,
-      address: req.body.address,
-    },
-  },
-  { new: true }
-)
-.then((updatedUser) => {
-  res.status(200).json(updatedUser.toJSON);
-})
-.catch((error) => {
-  console.log(error);
-  res.status(500).json(
+userRouter.put('/update/:id', async (req, res) => {
+  User.findByIdAndUpdate(
+    req.params.id,
     {
-      error:"Something wrong",
-    }
-    );
-  });
+      $set: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        address: req.body.address,
+      },
+    },
+    { new: true }
+  )
+    .then((updatedUser) => {
+      res.status(200).json(updatedUser.toJSON);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        error: 'Something wrong',
+      });
+    });
 });
- 
+
 module.exports = userRouter;
