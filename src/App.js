4b03f3c22b3d0be20f859612db981a5615357407
Michelle Tanoto <michelletanoto@Michelles-MacBook-Pro-3.local>
2020-09-24 16:52:39 +1000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
+// Entry File
 import React, { Fragment } from 'react';
 import {
   CssBaseline,
   withStyles,
 } from '@material-ui/core';
-
 import { BrowserRouter as Router, Route} from "react-router-dom";
+
+// Header
 import AppHeader from './components/AppHeader';
 import Info from './components/Info';
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import Login from './pages/Login/Login.js';
+
+// User
+import Register from './pages/Register';
+import Login from './pages/Login';
 import Logout from './pages/Logout';
-import Register from './pages/Register/Register.js';
-import FindHome from './pages/Search/FindHome.js';
-import Agent from './pages/Agent.js';
-import News from './pages/News.js';
-import NP from './pages/NP.js';
+import Home from './pages/Home';
+import Suburb from './pages/Suburb';
+import Agent from './pages/Agent';
+import News from './pages/News';
+import Property from './pages/Property';
+import ReadCurrentUser from './pages/User/read.js';
+import EditCurrentUser from './pages/User/edit.js';
+
+// Admin
 import AdminLogin from './pages/Admin/login.js';
 import AdminHome from './pages/Admin/home.js';
 import AdminEdit from './pages/Admin/edit.js';
-import ReadCurrentUser from './pages/User/read.js';
-import EditCurrentUser from './pages/User/edit.js';
 
 const styles = theme => ({
   main: {
@@ -39,15 +45,16 @@ const App = ({ classes }) => (
     <main className={classes.main}>
     <Router>
       <>
+      {/* User */}
       <Route path="/" exact component={Home} />
       <Route path="/home" exact component={Home} />
       <Route path="/login" exact component={Login} />
       <Route path="/logout" exact component={Logout} />
       <Route path="/register" exact component={Register} />
-      <Route path="/FindHome" exact component={FindHome} />
-      <Route path="/News" exact component={News} />
-      <Route path="/Agent" exact component={Agent} />
-      <Route path="/NP" exact component={NP} />
+      <Route path="/find/suburbs" exact component={Suburb} />
+      <Route path="/news" exact component={News} />
+      <Route path="/find/agents" exact component={Agent} />
+      <Route path="/find/properties" exact component={Property} />
       <Route path="/user/read" exact component={ReadCurrentUser}/>
       <Route path="/user/edit" exact component={EditCurrentUser}/>
       {/* Admin */}
